c658c28f339dcd504e4c541f404792370ca49a48
marcus <nacous13@gmail.com>
2024-10-03 20:47:26 -0300

    teste3

--- main.cpp
+++ main.cpp
@@ -1,16 +1,5 @@
 #include "Pessoa.hpp"
 
 int main(void) {
-	Pessoa Einstein(14, 3, 1879, "Einstein");
-	Pessoa Newton(4, 1, 1643, "Newton");
-
-	Einstein.Calc_idade(2, 10, 2024);
-	Newton.Calc_idade(2, 10, 2024);
-
-	// imprime o nome e idade
-	Einstein.imprime();
-	Newton.imprime();
-
-	getchar();
-	return 0;
+//escrevi e saí correndo pau no cu de quem ta lendo
 }
